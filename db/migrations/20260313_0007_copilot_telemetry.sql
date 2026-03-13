334943d3fac5f80ce684e9dee4867678f39ef7e5
Amritesh Dasari <dmamritesh@gmail.com>
2026-03-13 21:08:29 +0530

    Db init changes and review

--- db/migrations/20260313_0007_copilot_telemetry.sql
+++ db/migrations/20260313_0007_copilot_telemetry.sql
@@ -143,7 +143,7 @@ SELECT gen_random_uuid(), 'copilot_telemetry', 'default', '{"lookback_hours": 24
 WHERE NOT EXISTS (SELECT 1 FROM jobs WHERE job_type = 'copilot_telemetry');
 
 INSERT INTO job_schedules (schedule_id, job_id, cron_expr, next_run_at, enabled)
-SELECT gen_random_uuid(), j.job_id, '*/15 * * * *', NULL, true
+SELECT gen_random_uuid(), j.job_id, '*/60 * * * *', NULL, true
 FROM jobs j
 LEFT JOIN job_schedules js ON js.job_id = j.job_id
 WHERE j.job_type = 'copilot_telemetry' AND js.job_id IS NULL;
